48a725f73330a2792a721e2a5dd9e43b61c301a6
huyleminh <leminhhuy.hcmus@gmail.com>
2020-10-06 13:37:01 +0700

    commit: setup base operator

--- src/String/My-String.cpp
+++ src/String/My-String.cpp
@@ -1,25 +1,62 @@
 #include "My-String.h"
 
 str::String::String() {
-	this->_data = "";
+    this->_data = "";
 }
 
-str::String::String(const std::string& str)
-{
+str::String::String(const std::string &str) {
     this->_data = str;
 }
 
-str::String::String(const str::String& str)
-{
+str::String::String(const str::String &str) {
     this->_data = str._data;
 }
 
-str::String& str::String::operator =(const std::string& str) {
+str::String::String(const char* ch) {
+    this->_data = ch;
+}
+
+str::String &str::String::operator=(const std::string &str) {
     this->_data = str;
     return *this;
 }
 
-str::String& str::String::operator =(const str::String& str) {
+str::String &str::String::operator=(const str::String &str) {
     this->_data = str._data;
     return *this;
+}
+
+str::String str::String::operator+(const std::string &str) {
+    str::String result = "";
+    result._data += str;
+    return result;
+}
+
+str::String str::String::operator+(const str::String &str) {
+    str::String result = "";
+    result._data += str._data;
+    return result;
+}
+
+str::String &str::String ::operator+=(const std::string &str) {
+    this->_data += str;
+    return *this;
+}
+
+str::String &str::String ::operator+=(const str::String &str) {
+    this->_data += str._data;
+    return *this;
+}
+
+str::String &str::String ::operator+=(const char& ch) {
+    this->_data += ch;
+    return *this;
+}
+
+char& str::String::operator[](const int& pos) {
+    return this->_data[pos];
+}
+
+const char& str::String::operator[](const int& pos) const {
+    return this->_data[pos];
 }
--- src/String/My-String.h
+++ src/String/My-String.h
@@ -14,9 +14,23 @@ namespace str {
 		//Copy constructor
 		String(const std::string& str);
 		String(const str::String& str);
+		String(const char* ch);
 
+		//Assignment operator
 		String& operator =(const std::string& str);
 		String& operator =(const str::String& str);
+
+		//Member operator
+		String operator +(const std::string& str);
+		String operator +(const str::String& str);
+
+		String& operator +=(const std::string& str);
+		String& operator +=(const str::String& str);
+		String& operator +=(const char& ch);
+
+		char& operator [](const int& pos);
+		const char& operator [](const int& pos) const;
+
 	};
 }
 
